16e4f71eb7e3d2d29a76e8e36f78c59e76ffc6ae
ssaa2526 <dosbbq@gmail.com>
2025-07-18 13:24:46 +0900

    ✨ 영수증 보기 내용 추가
    사용자가 영수증 내역을 확인할 수 있는 기능 추가 영수증 내용 없을 시 안내 메시지  출력

--- atm.py
+++ atm.py
@@ -6,6 +6,8 @@
 
 balance = 10000
 
+receipts = []
+
 while True:
      num = input("사용하실 기능의 번호를 선택해주세요 (1.입금, 2.출금, 3.영수증 보기, 4.종료) : ") 
 
@@ -16,14 +18,20 @@ while True:
           deposit_amount = int(input('입금할 금액을 입력해주세요: '))
           balance  += deposit_amount 
           print(f'입금하신 금액{deposit_amount}원이고, 현재 잔액은{balance}원 입니다.')
+          receipts.append({'입금', deposit_amount, balance})
 
      if num == '2':
           withdraw_amout = int(input("출금할 금액을 입력해주세요."))
           withdraw_amout = min(balance,withdraw_amout)
           balance -= withdraw_amout 
           print(f"출금하신 금액은 {withdraw_amout}원이고, 현재 잔액은 {balance}원 입니다.")
+          receipts.append({'출금', withdraw_amout, balance})
 
      if num == '3':
-          pass
+          if receipts:
+             for i in receipts:
+                 print(f"{i[0]}: {i[1]}원 | 잔액: {i[2]}")
+          else:
+               print("영수증 내역이 없습니다.") 
 
 print(f'서비스를 종료합니다. 현재 잔액은 {balance}원 남았습니다.')
